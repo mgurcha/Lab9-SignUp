dc24fa3c9473a3e60008d4303362fd4e0697931f
Manjit Gurcha <mgurcha@csumb.edu>
2018-05-07 05:32:16 +0000

    Adding more questions

--- js/gradeQuiz.js
+++ js/gradeQuiz.js
@@ -13,6 +13,8 @@ $(document).ready(function(){
         //Get answers
         var answer1 = $("input[name='question1']").val().trim();
         var answer2 = $("input[name='question2']:checked").val();
+        var answer3 = $("input[name='question3']:checked").val();
+        var answer4 = $("input[name='question4']:checked").val();
         
         // console.log(answer1);
         // console.log(answer2);
@@ -36,6 +38,28 @@ $(document).ready(function(){
             incorrectAnswer($("#question2-feedback"));
         }
         $("#question2-feedback").append("The answer is <strong>Monte Rey</strong>" );
+        
+        if(answer3 === "A")
+        {
+            correctAnswer($("#question3-feedback"));
+        }
+        else
+        {
+            incorrectAnswer($("#question3-feedback"));
+        }
+        $("#question3-feedback").append("The answer is <strong>Seaside</strong>");
+        
+        
+        // Question 4
+        if(answer4 === "A")
+        {
+            correctAnswer($("#question4-feedback"));
+        }
+        else
+        {
+            incorrectAnswer($("#question4-feedback"));
+        }
+        $("#question4-feedback").append("The answer is <strong>Blue & Green</strong>");
         
         //Displays quiz score
         $("#score").html( score );
--- quiz.php
+++ quiz.php
@@ -11,6 +11,20 @@
     <input type="radio" name="question2" id="q2-3"  Value="C"/><label for='q2-3'>Monte Rey <br />
     <div id="question2-feedback" class="answer"></div><br />
 
+    <!--Question 3-->
+    Where is CSUMB located at?<br />
+    <input type="radio" name="question3" id="q3-1"  value="A"/><label for='q3-1'>Seaside <br />
+    <input type="radio" name="question3" id="q3-2"  value="B"/><label for='q3-2'>Salinas <br />
+    <input type="radio" name="question3" id="q3-3"  Value="C"/><label for='q3-3'>San Jose <br />
+    <div id="question3-feedback" class="answer"></div><br />
+    
+    <!--Question 4-->
+    What is CSUMB's colors?<br />
+    <input type="radio" name="question4" id="q4-1"  value="A"/><label for='q4-1'>Blue & Green <br />
+    <input type="radio" name="question4" id="q4-2"  value="B"/><label for='q4-2'>Blue & Red <br />
+    <input type="radio" name="question4" id="q4-3"  Value="C"/><label for='q4-3'>Gold & Blue <br />
+    <div id="question4-feedback" class="answer"></div><br />
+    
     <input type="submit" value="Submit" />
     
     <!--Will display the "loading" or "spinning" animated gif-->
